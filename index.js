4749bd3ef74c65bdf2898f55c42225246dcfac20
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-03 19:44:29 +0500

    kubernetes deployment

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var http = require('http');
 
 // create LINE SDK config from env variables
 const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN||config.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET||config.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
 };
 
 setInterval(function() {
